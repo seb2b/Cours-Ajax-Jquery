427f1f376509f5171f55dc5cf8f59e0fad731ffb
Sebastien Alliot <seballiot@MacBook-Pro-de-Sebastien.local>
2018-02-06 11:25:30 +0100

    Envoie sur github

--- traitement.php
+++ traitement.php
@@ -6,12 +6,16 @@ if(isset($_GET['id_post'])){
     $req = Connexion()->prepare('SELECT * FROM comments WHERE id_post =?');
     $req->execute(array($_GET['id_post']));
     $comments = $req->fetchAll();
-    ?><h3>Commentaires de l'article</h3><?php
+    ?>
+    <h3>Commentaires de l'article</h3>
+    <hr>
+    <?php
     foreach ($comments as $comment){
         ?>
         <p>
-            <strong></strong>
-        </p>
+            <strong><?= $comment->created_at;?></strong><br>
+            <?= $comment->content;?>
+        </p><br>
         <?php
     }
 }
